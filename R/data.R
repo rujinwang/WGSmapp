638c6544f0a13c87378f157e71e3a15cc797cb85
rujinwang <rujinwang1124@gmail.com>
2019-09-05 15:08:26 -0400

    ready for submission

---
+++ R/data.R
@@ -0,0 +1,25 @@
+#' GRanges with mappability scores for hg19
+#'
+#' GRanges of mappability track for 100-mers on the GRCh37/hg19 human reference
+#'         genome from ENCODE.
+#'
+#' @docType data
+#'
+#' @format A GRanges object with 21591667 ranges and mappability scores
+#'
+#' @keywords datasets
+#'
+"mapp_hg19"
+
+
+#' GRanges with mappability scores for hg38
+#'
+#' Use liftOver utility to convert hg19 coordinates to hg38
+#'
+#' @docType data
+#'
+#' @format A GRanges object with 21584930 ranges and mappability scores
+#'
+#' @keywords datasets
+#'
+"mapp_hg38"
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # WGSmapp
 This data package provides whole-genome mappability tracks on human hg19/hg38 assembly
+
+## Installation
+```r
+install.packages('devtools')
+devtools::install_github("rujinwang/WGSmapp")
+```
